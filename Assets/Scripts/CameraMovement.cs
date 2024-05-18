1f552e3e04a6a61c2f478cc03537ca7d86278a62
Nico2398 <nicolas.mienne.dev@gmail.com>
2024-05-18 19:17:07 +0200

    Add: Camera movement

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    [SerializeField] GameObject player;
+
+    float defaultZ = 0f;
+
+    void Start()
+    {
+        defaultZ = transform.position.z;
+    }
+
+    void Update()
+    {
+        Vector3 position = player.transform.position;
+        position.z = defaultZ;
+        position.y = Mathf.Max(0f, position.y);
+        transform.position = position;
+    }
+}
